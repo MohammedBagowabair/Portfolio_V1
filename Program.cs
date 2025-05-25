b15afcb32a58c575cca1d28b92014c71d1bafd66
MohammedBagowabair <159595929+MohammedBagowabair@users.noreply.github.com>
2025-05-25 15:07:36 +0300

    some publish changes

--- Program.cs
+++
@@ -1,6 +0,0 @@
-
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
-app.UseDefaultFiles(); // This serves index.html by default
-app.UseStaticFiles();  // This serves CSS, JS, images
-app.Run();
